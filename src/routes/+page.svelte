88ad3786dfbf6a0c612f9f1b463962676efa0534
Joswin <joswinpresten20@gmail.com>
2025-06-27 13:02:12 +0530

    Created qr code generator using svelte-qrcode library

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,30 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import QrCode from 'svelte-qrcode';
+
+	let value = $state('');
+	let linkValue = $derived(value.trim());
+</script>
+
+<h1 id="title">QR Code Generator</h1>
+
+<label for="link">Enter link:</label>
+<input type="text" bind:value name="link" />
+
+{#if value != ''}
+	<div>
+		<QrCode value={linkValue} />
+	</div>
+{/if}
+
+<style>
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+	}
+
+	#title {
+		text-align: center;
+		margin: 1rem;
+	}
+</style>
