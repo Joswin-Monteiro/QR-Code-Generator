32ef8370951bde519bb054aba4c4d00de4da455a
Joswin <joswinpresten20@gmail.com>
2025-06-27 15:10:27 +0530

    Created functions for functionality

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,19 +2,45 @@
 	import QrCode from 'svelte-qrcode';
 
 	let value = $state('');
-	let linkValue = $derived(value.trim());
+	let linkValue = $state('');
+	let generateQR = $state(false);
+	let isTextEmpty = $state(true);
+
+	function generate() {
+		generateQR = true;
+		if (value != '') {
+			isTextEmpty = false;
+			linkValue = value.trim();
+		} else {
+			console.log('Text is empty!');
+			isTextEmpty = true;
+		}
+	}
+
+	function handleKeyDown(event) {
+		if (event.key == 'Enter') generate();
+	}
 </script>
 
-<h1 id="title">QR Code Generator</h1>
+<div id="box">
+	<div id="box2">
+		<h1 id="title">QR Code Generator</h1>
 
-<label for="link">Enter link:</label>
-<input type="text" bind:value name="link" />
+		<label for="link">Enter link:</label>
+		<input type="text" bind:value name="link" onkeydown={handleKeyDown} />
+		<button onclick={generate}>Generate</button>
 
-{#if value != ''}
-	<div>
-		<QrCode value={linkValue} />
+		{#if generateQR}
+			{#if isTextEmpty}
+				<h1>Enter some text</h1>
+			{:else}
+				<div>
+					<QrCode value={linkValue} />
+				</div>
+			{/if}
+		{/if}
 	</div>
-{/if}
+</div>
 
 <style>
 	* {
@@ -26,5 +52,14 @@
 	#title {
 		text-align: center;
 		margin: 1rem;
+	}
+
+	#box {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		height: 100dvh;
+		border: 1px solid blue;
 	}
 </style>
